fd1f02196e6add5480717f6c9b410cac38380bef
akbarnurullaev <akbarnurullayev24@gmail.com>
2025-04-18 17:23:19 +0200

    added post method

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -1,13 +1,18 @@
+using Containers.Application;
+using Containers.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IContainerService, ContainerService>(
+    (_) => new ContainerService(connectionString)
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +21,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/api/containers", (IContainerService containerService) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    try
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        return Results.Ok(containerService.GetAllContainers());
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+app.MapPost("/api/containers", (IContainerService containerService, Container container) =>
+{
+    try
+    {
+        return containerService.CreateContainer(container) ? Results.Ok() : Results.BadRequest();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,72 @@
-﻿namespace Containers.Application;
+﻿using Containers.Models;
+using Microsoft.Data.SqlClient;
 
-public class ContainerService
+namespace Containers.Application;
+
+public class ContainerService : IContainerService
 {
+    private string _connectionString;
     
+    public ContainerService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = [];
+        
+        string sql = @"select * from Containers";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            
+            connection.Open();
+            
+            SqlDataReader reader = command.ExecuteReader();
+
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var container = new Container()
+                        {
+                            ID = reader.GetInt32(0),
+                            ContainerTypeId = reader.GetInt32(1),
+                            IsHazardious = reader.GetBoolean(2),
+                            Name = reader.GetString(3),
+                        };
+
+                        containers.Add(container);
+                    }
+                }
+            }
+            finally
+            {
+                reader.Close(); 
+            }
+        }
+        
+        return containers;
+    }
+
+    public bool CreateContainer(Container container)
+    {
+        const string insert = "insert into Containers (ContainerTypeId, IsHazardious, Name) values (@ContainerTypeId, @IsHazardious, @Name)";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(insert, connection);
+            command.Parameters.AddWithValue("@ContainerTypeId", container.ContainerTypeId);
+            command.Parameters.AddWithValue("@IsHazardious", container.IsHazardious);
+            command.Parameters.AddWithValue("@Name", container.Name);
+            
+            connection.Open();
+            
+            return command.ExecuteNonQuery() != -1;
+        }
+    }
 }
